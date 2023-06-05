0f698b43e88166cf8178df257aab389d1aca1643
Felipe Dias <felipedias.dev@gmail.com>
2023-06-04 23:24:20 -0300

    feat: :sparkles: add repository and usecase

---
+++ internal/infra/database/order_repository.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/felipedias-dev/fullcycle-go-expert-clean-architecture/internal/entity"
+)
+
+type OrderRepository struct {
+	DB *sql.DB
+}
+
+func NewOrderRepository(db *sql.DB) *OrderRepository {
+	return &OrderRepository{DB: db}
+}
+
+func (r *OrderRepository) Save(order *entity.Order) error {
+	stmt, err := r.DB.Prepare("INSERT INTO orders (id, price, tax, final_price) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(order.ID, order.Price, order.Tax, order.FinalPrice)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *OrderRepository) GetTotal() (int, error) {
+	var total int
+	err := r.DB.QueryRow("SELECT count(*) FROM orders").Scan(&total)
+	if err != nil {
+		return 0, err
+	}
+	return total, nil
+}
