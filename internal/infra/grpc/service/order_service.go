dbd9969735278f3c33c1592120fb634e047befdb
Felipe Dias <felipedias.dev@gmail.com>
2023-06-04 23:25:08 -0300

    feat: :sparkles: add REST and gRPC servers

---
+++ internal/infra/grpc/service/order_service.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"context"
+
+	"github.com/felipedias-dev/fullcycle-go-expert-clean-architecture/internal/infra/grpc/pb"
+	"github.com/felipedias-dev/fullcycle-go-expert-clean-architecture/internal/usecase"
+)
+
+type OrderService struct {
+	pb.UnimplementedOrderServiceServer
+	CreateOrderUseCase usecase.CreateOrderUseCase
+}
+
+func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase) *OrderService {
+	return &OrderService{CreateOrderUseCase: createOrderUseCase}
+}
+
+func (s *OrderService) CreateOrder(cx context.Context, in *pb.CreateOrderRequest) (*pb.CreateOrderResponse, error) {
+	dto := usecase.OrderInputDTO{
+		ID:    in.Id,
+		Price: float64(in.Price),
+		Tax:   float64(in.Tax),
+	}
+	output, err := s.CreateOrderUseCase.Execute(dto)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.CreateOrderResponse{
+		Id:         output.ID,
+		Price:      float64(output.Price),
+		Tax:        float64(output.Tax),
+		FinalPrice: float64(output.FinalPrice),
+	}, nil
+}
